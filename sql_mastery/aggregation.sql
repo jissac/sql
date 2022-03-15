5b170a65f2e180efbdf6ec050d2d25e3c36fa22e
jissac <jaeeysuk@gmail.com>
2022-03-15 14:12:48 -0500

    add files

--- sql_mastery/aggregation.sql
+++ sql_mastery/aggregation.sql
@@ -47,6 +47,40 @@ JOIN payment_methods pm
 ON (p.payment_method = pm.payment_method_id)
 JOIN invoices i USING (invoice_id)
 GROUP BY date, pm.name
-ORDER BY date 
+ORDER BY date;
 
 -- HAVING
+SELECT 
+	client_id,
+    SUM(invoice_total) AS total_sales,
+    COUNT(*) AS number_of_invoices
+FROM invoices
+GROUP BY client_id
+HAVING total_sales > 500 AND number_of_invoices > 5;
+
+-- customers located in Virgina who have spent more than $100
+use sql_store;
+SELECT
+    customer_id,
+    first_name,
+    last_name,
+    SUM(oi.quantity * oi.unit_price) AS total_sales
+FROM customers c
+JOIN orders o USING (customer_id)
+JOIN order_items oi USING (order_id)
+WHERE state = 'VA'
+GROUP BY customer_id, first_name,last_name
+HAVING total_sales > 100;
+
+-- ROLLUP clause
+use sql_invoicing;
+SELECT 
+	state,
+    city,
+    SUM(invoice_total) AS total_sales
+    FROM invoices
+    JOIN clients c USING (client_id)
+    GROUP BY state, city WITH ROLLUP
+    
+
+
